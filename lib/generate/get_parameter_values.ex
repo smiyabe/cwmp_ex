e64dfe2ef03418534d41560d10e9ead200fed312
Satoshi Miyabe <miyabe@bbsite.jp>
2020-11-10 16:48:59 +0900

    Correspondence of the model that fails with "Parse xml string failed." When there is no arrayType.

--- lib/generate/get_parameter_values.ex
+++ lib/generate/get_parameter_values.ex
@@ -3,7 +3,8 @@ defimpl CWMP.Protocol.Generate, for: CWMP.Protocol.Messages.GetParameterValues d
 
   def generate(req) do
     params=for p <- req.parameters, do: element(p.type, p.name)
-    element("cwmp:GetParameterValues", [element(:ParameterNames,params)])
+    count=params |> length
+    element("cwmp:GetParameterValues", [element(:ParameterNames,%{arrayType: "string[#{count}]"},params)])
   end
 end
 
